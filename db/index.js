303f4c59c33a78c3983e88b200698149a5f0c7ae
Brandon Albright <brandonalbright@brandons-air-2.lan>
2021-01-06 20:38:55 -0600

    building tables test

--- db/index.js
+++ db/index.js
@@ -1,11 +1,14 @@
 // Connect to DB
 const { Client } = require('pg');
 const DB_NAME = 'mango'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
 
+// async function getAllProducts() {
+
+// }
 // export
 module.exports = {
   client,
