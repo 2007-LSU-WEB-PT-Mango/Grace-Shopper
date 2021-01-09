09388b80e71d826079c4b3f892d06e97e69d2bbc
Sophia Dooley <sophiadooley865@gmail.com>
2021-01-09 12:01:00 -0600

    product api moved into correct folder

--- src/api/index.js
+++ src/api/index.js
@@ -9,31 +9,5 @@ export async function getSomething() {
   }
 }
 
-///^^^^idk what that top part is?? Do we need that?????
 
-const apiRouter = require("express").Router();
 
-apiRouter.get("/products", async (req, res, next) => {
-  try {
-    const allProducts = await getAllProducts();
-
-    res.send(allProducts);
-    next();
-  } catch (error) {
-    console.log("Error requesting products");
-    next(error);
-  }
-});
-
-apiRouter.get("/products/:id", async (req, res, next) => {
-  const { productId } = req.params;
-  try {
-    const products = getProduct(productId);
-
-    res.send({ products });
-  } catch (error) {
-    next(error);
-  }
-});
-
-module.exports = apiRouter;
