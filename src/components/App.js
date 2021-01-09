c93601b7d17dc4918685c0ab0006cbf250abf1bd
Brandon Albright <brandonalbright@brandons-air-2.lan>
2021-01-08 18:44:53 -0600

    fixing merge conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-<<<<<<< HEAD
 import { AlbumsList } from '../components';
 
 import { getSomething } from '../api';
-=======
-import {
-  getSomething
-} from '../api';
->>>>>>> database
 
 const App = () => {
   const [message, setMessage] = useState('');
 
-<<<<<<< HEAD
   // useEffect(() => {
   //   getSomething()
   //     .then((response) => {
@@ -33,24 +26,3 @@ const App = () => {
 };
 
 export default App;
-=======
-  useEffect(() => {
-    getSomething()
-      .then(response => {
-        setMessage(response.message);
-      })
-      .catch(error => {
-        setMessage(error.message);
-      });
-  });
-
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <h2>{ message }</h2>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> database
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export { default as App } from './App';
 export { default as Album } from './Album';
 export { default as AlbumsList } from './AlbumsList';
-=======
-export { default as App } from './App';
->>>>>>> database
