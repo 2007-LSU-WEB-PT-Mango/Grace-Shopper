cba984ea6680b24dc3313e4db4ea144d2d5d713e
Brandon Albright <brandonalbright@brandons-air-2.lan>
2021-01-11 20:20:58 -0600

    working on default db imageurl

--- db/init_db.js
+++ db/init_db.js
@@ -25,14 +25,14 @@ async function dropTables() {
 async function buildTables() {
   console.log("building tables");
   try {
-    // need to get default image url
+    
     await client.query(`
       CREATE TABLE users(
         id SERIAL PRIMARY KEY,
         "firstName" VARCHAR(255) NOT NULL,
         "lastName" VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
-        "imageURL" VARCHAR(255) DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/12in-Vinyl-LP-Record-Angle.jpg/1920px-12in-Vinyl-LP-Record-Angle.jpg',
+        "imageURL" VARCHAR(255),
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) UNIQUE NOT NULL,
         "isAdmin" BOOLEAN NOT NULL DEFAULT false
@@ -43,8 +43,8 @@ async function buildTables() {
         name VARCHAR(255) NOT NULL,
         description TEXT NOT NULL,
         price INTEGER NOT NULL,
-        "imageURL" VARCHAR(255),
-        "inStock" BOOLEAN NOT NULL DEFAULT false,
+        "imageURL" VARCHAR(500) DEFAULT "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/12in-Vinyl-LP-Record-Angle.jpg/1920px-12in-Vinyl-LP-Record-Angle.jpg",
+        "inStock" BOOLEAN DEFAULT false NOT NULL,
         category VARCHAR(255) NOT NULL
       );
 
@@ -85,6 +85,13 @@ async function populateInitialData() {
         "https://images-na.ssl-images-amazon.com/images/I/81dUVKQDBEL._SL1200_.jpg",
       inStock: true,
       category: "rock",
+    });
+
+    const productThree = await createProduct({
+      name: "test",
+      description: "by unknown",
+      price: 15,
+      category: "unknown",
     });
 
     console.log("success creating products!");
