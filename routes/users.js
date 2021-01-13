50296c4144b998587cd82336579a626a2e5c79ff
Cory Boudreaux <cboud31@gmail.com>
2021-01-13 13:57:43 -0600

    User registration  working locally in Postman. Build register API route, createUser db method

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,37 @@
 // GET   /users/me (*)
 
 const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const { checkUsername, createUser } = require('../db/index');
 
 usersRouter.post('/register', async (req, res, next) => {
   try {
-    res.json({ message: 'This route will register and create a new user.' });
+    //   1. De-structure the req.body (firstName, lastName, email, username, password).
+    const { firstName, lastName, email, username, password } = req.body;
+
+    //   2. Check if username exists.
+    const user = await checkUsername(username);
+
+    if (user.length !== 0) {
+      return res.send({
+        Warning: 'Username already exists!',
+      });
+    }
+
+    //   3. Bcrypt the user's password
+    //   4. Enter new user in db.
+    const newUser = await createUser({
+      firstName,
+      lastName,
+      email,
+      username,
+      password,
+    });
+
+    //   5. Generate a JWT.
+
+    console.log(newUser);
+    res.json(newUser);
   } catch (error) {
     next(error);
   }
