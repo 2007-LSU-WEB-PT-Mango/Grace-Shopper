e280ff2c53204e72514fd3e3734d2262bc669762
Brandon Albright <brandon4692@gmail.com>
2021-01-25 21:57:21 -0600

    routes updated with team

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const apiRouter = require('express').Router();
+const { default: axios } = require('axios');
 const {
   getAllProducts,
   getProduct,
@@ -6,9 +7,15 @@ const {
   getAllOrders,
   createOrder,
   cancelOrder,
+  getOrdersbyUser,
+  addProductToOrder,
+  destroyOrderProduct,
+  updateOrderProduct,
+  completeOrder
 } = require('../db/index');
 const verifyToken = require('../middleware/verifyToken');
 
+// get all products
 apiRouter.get('/products', async (req, res, next) => {
   try {
     const allProducts = await getAllProducts();
@@ -20,6 +27,7 @@ apiRouter.get('/products', async (req, res, next) => {
   }
 });
 
+// get product by id
 apiRouter.get('/products/:id', async (req, res, next) => {
   const { id } = req.params;
 
@@ -32,51 +40,30 @@ apiRouter.get('/products/:id', async (req, res, next) => {
   }
 });
 
-apiRouter.get('/ordertest/:id'),
+// get all orders by userID
+apiRouter.get('/orders/:userID', verifyToken, 
   async (req, res, next) => {
-    const { id } = req.params;
+    const { userID } = req.params;
 
     try {
-      const orders = await getOrderByID(id);
+      const orders = await getOrdersbyUser(userID);
       res.send(orders);
     } catch (error) {
       next(error);
     }
-  };
+  });
 
-apiRouter.patch('/orders/:orderId', verifyToken, async (req, res, next) => {
-  const { status, userId } = req.body;
-  console.log('The req.body is', req.body);
-
-  const update = {};
-
-  if (status) {
-    updateFields.status = status;
-  }
-
-  if (userId) {
-    updateFields.userId = userId;
-  }
-
+// changes order status to completed
+apiRouter.patch('/orders/complete/:orderId', verifyToken, async (req, res, next) => {
   try {
-    const order = await getOrderByID(userId);
-    console.log('the original order is', order);
-
-    console.log('The update fields are', update);
-
-    if (order.id === orderId) {
-      const updateOrder = await updateOrderProduct(userId, update);
-      res.send({ order: updateOrder });
-    } else {
-      next({
-        message: 'Error updating order',
-      });
-    }
+    const order = await completeOrder(req.params.orderId); //unsure yet what function will be called!
+    res.send(order);
   } catch (error) {
     throw error;
   }
 });
 
+// changes order status to cancelled
 apiRouter.delete('/orders/:orderId', verifyToken, async (req, res, next) => {
   try {
     const order = await cancelOrder(req.params.orderId); //unsure yet what function will be called!
@@ -86,74 +73,48 @@ apiRouter.delete('/orders/:orderId', verifyToken, async (req, res, next) => {
   }
 });
 
-// PATCH /order_products/:orderProductId (**)
-//Update the quantity or price on the order product
 
+// updating quantity in order/cart
 apiRouter.patch(
-  '/orders_products/:orderProductId',
+  '/orderedproducts/updatequantity',
   verifyToken,
   async (req, res, next) => {
-    const { quantity, price } = req.body;
+    const { orderID, productID, quantity } = req.body;
     console.log('The req.body is', req.body);
 
-    const update = {};
-
-    if (quantity) {
-      updateFields.status = quantity;
-    }
-
-    if (price) {
-      updateFields.userId = price;
-    }
-
     try {
-      const orderProduct = await getOrderByID(quantity, price);
-      console.log('the new quantity or price is', orderProduct);
-
-      console.log('The updated quantity or price is', update);
-
-      if (order.id === orderId) {
-        const updateOrder = await updateOrder(orderProduct, update);
-        res.send({ order: updateOrder });
-      } else {
-        next({
-          message: 'Error updating order',
-        });
-      }
+      const orderProduct = await updateOrderProduct(orderID, productID, quantity);
+      res.send(orderProduct);
     } catch (error) {
       throw error;
     }
   }
 );
 
-//  POST /orders/:orderId/products (**)
-//Add a single product to an order (using order_products).
-//Prevent duplication on ("orderId", "productId") pair.
-//If product already exists on order, increment quantity and update price.
-
+// add product to order
 apiRouter.post(
-  '/orders/:orderId/products',
+  '/orders/:orderId/:productId',
   verifyToken,
   async (req, res, next) => {
-    const { orderId } = req.params;
+    const { orderId, productId } = req.params;
     try {
-      const newProduct = await getOrderByProductId(orderId, req.body);
-      res.send(newProduct);
+      const newProduct = await addProductToOrder(orderId, productId);
+      console.log("newProduct:", newProduct)
+      res.send({"success": "true"});
     } catch (error) {
       next(error);
     }
   }
 );
 
-//DELETE /order_products/:orderProductId (**)
-//Remove a product from a order, use hard delete
-
+// delete product from order
 apiRouter.delete(
-  '/order_products/:orderProductId',
+  '/order_products/:orderID/:productID',
   verifyToken,
   async (req, res, next) => {
+    const { orderID, productID } = req.params;
     try {
-      const order = await destroyOrderProduct(req.params.orderProductId);
+      const order = await destroyOrderProduct(orderID, productID);
       res.send(order);
     } catch (error) {
       throw error;
@@ -161,13 +122,5 @@ apiRouter.delete(
   }
 );
 
-// Test route to create orders: POST /orders (*)
-apiRouter.post('/orders', verifyToken, async (req, res, next) => {
-  try {
-    const order = await createOrder(req.params.status, req.params.id);
-    res.send(order);
-  } catch (error) {
-    throw error;
-  }
-});
+
 module.exports = apiRouter;
